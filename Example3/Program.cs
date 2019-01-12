484f27b17965920bca5fa6d71e4d3959326d038b
Mike Hurley <mhurley@hurley-consulting.io>
2019-01-12 14:09:52 -0500

    practice01

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -15,21 +15,21 @@ namespace Example3
             string sampleString = "This is a string variable"; //string contains alphanumeric chatacters (text)
             Console.WriteLine("sampleString is equal to " + sampleString);
 
-            sampleString = "I have updated this variable!";
+            sampleString = "I have updated this variable again, on Saturday!";
             Console.WriteLine("we changed sampleString's value to " + sampleString);
 
-            int sampleInteger = 0; //int contains whole numbers only
+            int sampleInteger = 7; //int contains whole numbers only
             Console.WriteLine("sampleInteger is equal to " + sampleInteger);
 
-            sampleInteger = -4000;
+            sampleInteger = -7000;
             Console.WriteLine("we changed sampleInteger's value to " + sampleInteger);
 
-            double sampleDecimal = 33.3334; //double conatins a decimal number - decimal and float are similar types
+            double sampleDecimal = 77.3434; //double conatins a decimal number - decimal and float are similar types
             Console.WriteLine("sampleDecimal is equal to " + sampleDecimal);
 
             object a = 1;  // object can contain anything
 
-            var b = 33.33;  //var is shortcut for creating variables.  it's type is determined by assignment (value)
+            var b = 75.75;  //var is shortcut for creating variables.  it's type is determined by assignment (value)
 
             int someNumber;
             someNumber = sampleInteger;
