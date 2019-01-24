0fa2cbc6d703252e6ebdcfb099a3fac34bbad0bd
Mike Hurley <mhurley@hurley-consulting.io>
2019-01-24 14:27:11 -0500

    added perimeter functionality

--- Area-of-Rectangle/Program.cs
+++ Area-of-Rectangle/Program.cs
@@ -18,12 +18,21 @@ namespace Area_of_Rectangle
             double area = CalculateArea(height, width);
 
             Console.WriteLine("A rectangle with a height of " + height + " and width of " + width + " has an area of " + area);
+
+            int perimeter = CalculatePerimeter(height, width);
+            Console.WriteLine("A rectangle with a height of " + height + " and width of " + width + " has a perimeter of " + perimeter);
+
             Console.ReadLine();
         }
 
         public static double CalculateArea(int height, int width)
         {
             return (double)height * width;
+        }
+
+        public static int CalculatePerimeter(int height, int width)
+        {
+            return 2 * height + 2 * width;
         }
     }
         
