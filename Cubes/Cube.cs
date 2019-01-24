91a196d50bb260baec1c60178fbaf5c9e98b34bd
Mike Hurley <mhurley@hurley-consulting.io>
2019-01-24 15:11:41 -0500

    class practice

---
+++ Cubes/Cube.cs
@@ -0,0 +1,32 @@
+﻿namespace Cubes
+{
+    //this is a blue print of a Cube
+    public class Cube
+    {
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        //constructor
+        public Cube(int length, int height, int width)
+        {
+            Length = length;
+            Height = height;
+            Width = width;
+        }
+
+        //area
+        public double CalcualteArea()
+        {
+            return (double)Height * Width * Length;
+        }
+
+        public int CalculateSurfaceArea()
+        {
+            var smallSideArea = Length * Width;
+            var longSideArea = Length * Height;
+
+            return smallSideArea * 2 + longSideArea * 4;
+        }
+    }
+}
---
+++ Cubes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Cubes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a height: ");
+            string enteredHeight = Console.ReadLine();
+
+            Console.Write("Enter a width: ");
+            string enteredWidth = Console.ReadLine();
+
+            Console.Write("Enter a length: ");
+            string enteredLength = Console.ReadLine();
+
+            var cube1 = new Cube(int.Parse(enteredLength), int.Parse(enteredHeight), int.Parse(enteredWidth));
+            //cube1.Height = 3;
+            //cube1.Width = 5;
+            //cube1.Length = 10;
+
+            Console.WriteLine("we now have a cube with the length of " + cube1.Length + " and a width of " + cube1.Width + " and a height of " + cube1.Height);
+
+            var area = cube1.CalcualteArea();
+
+            Console.WriteLine("The area of my cube is " + area);
+
+            var surfaceArea = cube1.CalculateSurfaceArea();
+            Console.WriteLine("The surface area of my cube is " + surfaceArea);
+
+            Console.ReadLine();
+        }
+    }
+}
