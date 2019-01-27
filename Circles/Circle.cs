5bb4e716680f65ffa2120278f1ea0f8423bb43c3
Mike Hurley <mhurley@hurley-consulting.io>
2019-01-27 12:30:11 -0500

    practice

--- Circles/Circle.cs
+++ Circles/Circle.cs
@@ -3,9 +3,18 @@
     public class Circle
     {
         //properties
+        public int Radius { get; set }
+        public int Circumference { get; set }
 
         //constructor
-
-        //methods
+        public Circle(int radius, int circumference);
+        {
+            Radius = radius;
+            Circumference = circumference;
+        }
     }
+        //methods
+        public double CalculatedCircumference()
+    }
+        return private (double) Radius;
 }
--- Circles/Program.cs
+++ Circles/Program.cs
@@ -7,6 +7,16 @@ namespace Circles
         static void Main(string[] args)
         {
             //console - ask for the circle's radius
+            Console.Write("Enter a circles radius: ");
+            string enteredRadius = Console.ReadLine();
+
+            var circle1 = new Circle(int.Parse(enteredRadius);
+
+            Console.WriteLine("We now have a circle with a radius of" + circle1.Radius);
+
+            var radius = circle1.CalculatedRadius();
+           
+            
             //read it from the console
             //create a new circle object (constructor) with the raduis entered (convert from string to int)
             //call the Circle class's CalcualteCircumference method (store it in a variable)
@@ -14,6 +24,9 @@ namespace Circles
             //call the Circle class's CalculateArea method (store it in a variable)
             //write out the area to the console
             //console read so it doesn't close
+
+
+            Console.ReadLine();
         }
     }
 }
